5264a04810f41803d46c45bf1250174838ae7d4d
infotronic218 <infotronic218@gmail.com>
2024-05-31 23:55:45 +0200

    Update the first code

---
+++ nRF7002_dk_led/src/main.c
@@ -0,0 +1,15 @@
+#include<zephyr/kernel.h>
+#include<zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+
+int main()
+{
+    while(1)
+    {
+         k_msleep(2000);
+         LOG_INF("Hello world !");
+    }
+    return 0 ;
+}
