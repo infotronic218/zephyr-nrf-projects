9f5fe5354796bbe809252d7c88a997f9caa8386b
infotronic218 <infotronic218@gmail.com>
2024-06-09 16:22:52 +0200

    Update the blinky program

--- nRF7002_dk_led/src/main.c
+++ nRF7002_dk_led/src/main.c
@@ -1,16 +1,29 @@
 #include<zephyr/kernel.h>
 #include<zephyr/logging/log.h>
-
+#include<zephyr/drivers/gpio.h>
+#include<zephyr/device.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
+
+static struct gpio_dt_spec led_dev = GPIO_DT_SPEC_GET(DT_NODELABEL(led1), gpios);
 
 
 int main()
 {
+    if(!device_is_ready(led_dev.port)){
+        LOG_ERR("Device is not  ready");
+        return -1 ;
+    }
+
+    gpio_pin_configure_dt(&led_dev, GPIO_OUTPUT_ACTIVE);
+
     while(1)
     {
         k_msleep(2000);
         LOG_INF("Hello world !");
-        printk("hello world ");
+        gpio_pin_set_dt(&led_dev, 0);
+        k_msleep(1000);
+        gpio_pin_set_dt(&led_dev, 1);
     }
     return 0 ;
 }
